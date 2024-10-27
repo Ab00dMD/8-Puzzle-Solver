5d58ceb4854166e8012de4d71978e7d1017cb440
Ab00dMD <abdalrhmanmohamed94@gmail.com>
2024-10-27 21:53:49 +0300

    DFS

---
+++ Node.py
@@ -0,0 +1,7 @@
+class Node:
+
+    def __init__(self,state,parent,children,direction):
+        self.state = state
+        self.parent = parent
+        self.children = children
+        self.direction = direction
