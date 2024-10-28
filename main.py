0b92eaea143fb6c513301649ec77f071b6db275b
Ahmad Hassan <mi010203269@gmail.com>
2024-10-28 17:58:06 +0300

    add streanlit

--- main.py
+++ main.py
@@ -3,25 +3,40 @@ from algorithms.dfs import DFS
 from algorithms.ids import IDS
 from algorithms.bfs import BFS
 from algorithms.a_star import AStar
+import streamlit as st
 
-def main():
-    print("Welcome to the 8-Puzzle Solver! please input the initial state of the board:")
-    number = input()
-    
-    print_board(number)
-    
-    children = get_children(number)
-    
-    print("Children of the current state:")
-    for child in children:
-        print(child)
 
-if __name__ == "__main__":
-    #ids = IDS("806547231")
-    #print(ids.search())
+def build():
+    st.title("8 Puzzle Solver")
+    st.write("This is a simple 8 puzzle solver using different algorithms")
+    algorithm = render_sidebar()
+    st.write("Algorithm: ", algorithm)
+    board = st.text_area("Enter the board", "1 2 3\n4 5 6\n7 8 0")
+    board = [[int(x) for x in row.split()] for row in board.split('\n')]
+    print_board(board)
+    if algorithm == "DFS":
+        print("DFS")
+    else:
+        print("IDS")
 
-    #dfs = DFS("806547231")
-    #print(dfs.search())
-    pass
+
+def render_sidebar():
+    st.sidebar.title("Algorithms")
+    st.sidebar.subheader("Choose an algorithm")
+    algorithm = st.sidebar.selectbox(
+        "Algorithm",
+        ["DFS", "IDS", "BFS", "A*"]
+    )
+    return algorithm
+
+
+
+
+
+
+
+
+
+build()
     
         
